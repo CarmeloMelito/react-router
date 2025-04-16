9c4c04db37adab60aa27cf6f0b43292b547279de
CarmeloMelito <melitocarmelo211@gmail.com>
2025-04-16 18:38:28 +0200

    feat:create route and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Post from "./pages/Post";
+import ChiSiamo from "./pages/ChiSiamo";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chi-siamo" element={<ChiSiamo />} />
+          <Route path="/post" element={<Post />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
